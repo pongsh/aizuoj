2c7acd0e40bf0a18555140d2fe2dffdfb0e21e08
Patrick Ong <p.ongsh@gmail.com>
2013-04-18 20:49:56 +0800

    Added 0103.

---
+++ volume_1/0103.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+
+    string event;
+
+    for (int n = 0; n != N; ++n) {
+        
+        int runners = 0, points = 0, out = 0;
+        
+        while (out < 3) {
+            cin >> event;
+            if (event == "OUT") out++;
+            if (event == "HIT") {
+                if (runners == 3) {
+                    points++;
+                }    
+                else {
+                    runners++;
+                }
+            }
+            if (event == "HOMERUN") {
+                points += runners + 1;
+                runners = 0;
+            }
+        }
+        cout << points << endl;
+    }
+}
